2db009a4fd383f07adadeda13fed0da274dbf80e
Elsa Yang <elsayang0503@gmail.com>
2023-04-18 12:46:47 +0800

    updated time

--- webscaper-chinatimes.py
+++ webscaper-chinatimes.py
@@ -1,5 +1,4 @@
 import requests
-import time, random
 from fake_useragent import UserAgent
 import pandas as pd
 from bs4 import BeautifulSoup as bs
@@ -17,8 +16,8 @@ focus_news = soup_chinatimes.select('li.col-md-6')
 title = [i.select('h3.title')[0].text for i in focus_news]
 urls = [i.select('h3.title')[0].a['href'] for i in focus_news]
 category = [i.select('div.category')[0].text for i in focus_news]
-time = [i.select('time')[0].text for i in focus_news]
+time_ = [i.select('time')[0].text for i in focus_news]
 
-zipped = zip(title,urls,category,time)
+zipped = zip(title,urls,category,time_)
 df_chinatimes = pd.DataFrame(list(zipped), columns=['title', 'url','tags','time'])
 print(df_chinatimes)
